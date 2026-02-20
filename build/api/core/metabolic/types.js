d7992abe398145c7d84363ab8f7ef0d20d56973f
lucaskurita7-max <lucaskurita7@gmail.com>
2026-02-19 22:29:01 -0300

    remove unused folders

--- build/api/core/metabolic/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Tipos de dados utilizados no núcleo metabólico V1.
- * Estes tipos são independentes de qualquer framework e podem ser
- * importados tanto na camada de serviços quanto na UI.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
